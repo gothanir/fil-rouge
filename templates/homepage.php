b916b8ae6ec96a1336129c792f07a7f3e6b73180
gothanir <derred588@gmail.com>
2023-02-17 12:32:08 +0100

    message

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,5 +1,5 @@
 <?php $title = "fil rouge"; ?>
-<link rel="stylesheet" type="text/css" href="css/homepage.css"/>
+
 <?php ob_start(); ?>
 <header>
     <h1>Fil-rouge</h1>
@@ -8,12 +8,33 @@
     <p>Launcher</p>
     <p>Support</p>
     <div class="end">
-        <img class="logoConnection" src="images/login.png">
+        <img class="logoConnection" src="assets/images/login.png">
         <p>Se connecter</p>
     </div>
 </header>
 <nav>
-
+    <ul>
+        <li>
+            <a href=""  class="sm">Jeu</a>
+            <ul>
+                <li><a href="">categorie 1</a></li>
+                <li><a href="">categorie 2</a></li>
+                <li><a href="">categorie 3</a></li>
+                <li><a href="">categorie 4</a></li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href=""  class="sm">DLC</a>
+            <ul>
+                <li><a href="">categorie 1</a></li>
+                <li><a href="">categorie 2</a></li>
+                <li><a href="">categorie 3</a></li>
+                <li><a href="">categorie 4</a></li>
+            </ul>
+        </li>
+    </ul>
 </nav>
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
